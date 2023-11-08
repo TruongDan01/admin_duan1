6628b54911d79b3c07631aa9501d0bcc40745adb
Th@2.dev <99267273+Webww@users.noreply.github.com>
2023-11-09 01:38:47 +0700

    update

--- assets/js/Validation.js
+++ assets/js/Validation.js
@@ -18,7 +18,7 @@ function Validator(options) {
 
         // Lấy ra các rules của selector
         var rules = selectorRules[rule.selector];
-        
+
         // Lặp qua từng rule & kiểm tra
         // Nếu có lỗi thì dừng việc kiểm
         for (var i = 0; i < rules.length; ++i) {
@@ -34,7 +34,7 @@ function Validator(options) {
             }
             if (errorMessage) break;
         }
-        
+
         if (errorMessage) {
             errorElement.innerText = errorMessage;
             getParent(inputElement, options.formGroupSelector).classList.add('invalid');
@@ -54,22 +54,69 @@ function Validator(options) {
             e.preventDefault();
 
             var isFormValid = true;
+            var errorTab = null;
 
-            options.rules.forEach(function (rule) {
-                var inputElement = formElement.querySelector(rule.selector);
-                var isValid = validate(inputElement, rule);
-                if (!isValid) {
-                    isFormValid = false;
+            if (options.tabs) {
+                var hasError = false; // To track if there's an error
+
+                options.rules.forEach(function (rule) {
+                    var inputElement = formElement.querySelector(rule.selector);
+                    var isValid = validate(inputElement, rule);
+                    if (!isValid && !errorTab) {
+                        hasError = true; // Set the hasError flag
+                        // Remove 'active' class from the current active tab link
+                        var activeTabLink = formElement.querySelector('.nav-link.active');
+                        if (activeTabLink) {
+                            activeTabLink.classList.remove('active');
+                        }
+
+                        errorTab = inputElement.closest('.tab-pane');
+                    }
+                });
+
+                // Remove 'show' and 'active' classes from all tabs
+                var allTabs = formElement.querySelectorAll('.tab-pane');
+                allTabs.forEach(function (tab) {
+                    tab.classList.remove('show', 'active');
+                });
+
+                if (errorTab) {
+                    // Activate and show only the tab with errors
+                    errorTab.classList.add('show', 'active');
+
+                    // Activate the corresponding tab link in the <ul> by its aria-controls attribute
+                    var tabLink = formElement.querySelector('[aria-controls="' + errorTab.id + '"]');
+                    if (tabLink) {
+                        tabLink.classList.add('active');
+                    }
                 }
-            });
+
+                // Scroll to the top of the page if there's an error
+                if (hasError) {
+                    window.scrollTo({
+                        top: 0,
+                        behavior: 'smooth'
+                    });
+                }
+            } else {
+                // Validation for non-tabbed form
+                options.rules.forEach(function (rule) {
+                    var inputElement = formElement.querySelector(rule.selector);
+                    var isValid = validate(inputElement, rule);
+                    if (!isValid) {
+                        isFormValid = false;
+                    }
+                });
+            }
+
 
             if (isFormValid) {
 
                 if (typeof options.onSubmit === 'function') {
                     var enableInputs = formElement.querySelectorAll('[name]');
                     var formValues = Array.from(enableInputs).reduce(function (values, input) {
-                        
-                        switch(input.type) {
+
+                        switch (input.type) {
                             case 'radio':
                                 values[input.name] = formElement.querySelector('input[name="' + input.name + '"]:checked').value;
                                 break;
@@ -93,9 +140,7 @@ function Validator(options) {
                         return values;
                     }, {});
                     options.onSubmit(formValues);
-                }
-
-                else {
+                } else {
                     formElement.submit();
                 }
             }
@@ -120,7 +165,7 @@ function Validator(options) {
                     var errorElement = getParent(inputElement, options.formGroupSelector).querySelector(options.errorSelector);
                     errorElement.innerText = '';
                     getParent(inputElement, options.formGroupSelector).classList.remove('invalid');
-                } 
+                }
             });
         });
     }
@@ -131,7 +176,7 @@ Validator.isRequired = function (selector, message) {
     return {
         selector: selector,
         test: function (value) {
-            return value ? undefined :  message || 'Vui lòng nhập trường này'
+            return value ? undefined : message || 'Vui lòng nhập trường này'
         }
     };
 }
@@ -141,7 +186,7 @@ Validator.isEmail = function (selector, message) {
         selector: selector,
         test: function (value) {
             var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-            return regex.test(value) ? undefined :  message || 'Trường này phải là email';
+            return regex.test(value) ? undefined : message || 'Trường này phải là email';
         }
     };
 }
@@ -150,7 +195,7 @@ Validator.minLength = function (selector, min, message) {
     return {
         selector: selector,
         test: function (value) {
-            return value.length >= min ? undefined :  message || `Vui lòng nhập tối thiểu ${min} kí tự`;
+            return value.length >= min ? undefined : message || `Vui lòng nhập tối thiểu ${min} kí tự`;
         }
     };
 }
@@ -160,6 +205,35 @@ Validator.isConfirmed = function (selector, getConfirmValue, message) {
         selector: selector,
         test: function (value) {
             return value === getConfirmValue() ? undefined : message || 'Giá trị nhập vào không chính xác';
+        }
+    }
+}
+
+Validator.isNumber = function (selector, message) {
+    return {
+        selector: selector,
+        test: function (value) {
+            if (value.length === 0) {
+                return message || 'Không được bỏ trống';
+            }
+
+            if (isNaN(value)) {
+                return message || '* Vui lòng nhập số !';
+            }
+        }
+    }
+}
+
+Validator.isDiscount = function (selector, getCost, message) {
+    return {
+        selector: selector,
+        test: function (value) {
+            if (isNaN(value)) {
+                return '* Vui lòng nhập số !';
+            }
+            if (value >= getCost()) {
+                return message || '* Giá giảm phải nhỏ hơn giá gốc !';
+            }
         }
     }
 }
